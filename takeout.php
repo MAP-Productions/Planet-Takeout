d5e1bb26d9237bb20b2361ad106d3eb123570de8
James Burns <james@metalab.harvard.edu>
2012-09-27 19:48:17 +0000

    Updated proxy

--- takeout.php
+++ takeout.php
@@ -4,24 +4,25 @@
 		$fields = json_decode($post_data,true);	
 		
 		
-		$url = 'http://staging.zeega.org/items';
+		$url = 'http://staging.zeega.org/api/items';
 		
 		//url-ify the data for the POST
-		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		/*foreach($fields as $key=>$value) { 
+			$fields_string .= $key.'='.urlencode($value).'&'; 
+		}
 		rtrim($fields_string, '&');
-		
+		*/
+
 		//open connection
 		$ch = curl_init();
 		
 		//set the url, number of POST vars, POST data
 		curl_setopt($ch,CURLOPT_URL, $url);
-		curl_setopt($ch,CURLOPT_POST, count($fields));
-		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		curl_setopt($ch,CURLOPT_POSTFIELDS, http_build_query($fields));
 		
 		//execute post
 		$result = curl_exec($ch);
-		
-		print_r($result);
+		print_r($result);	
 		//close connection
 		curl_close($ch);
 		
